8ad545a43cd43bb8ba49d10b3d70e9dd90671dd2
wangyz1999 <yunzhewa@usc.edu>
2022-05-10 23:59:06 -0400

    update

--- run.bat
+++ run.bat
@@ -2,3 +2,6 @@ python train.py --run_name noher_dense
 python train.py --run_name her_dense --use_her
 python train.py --run_name noher_sparse --sparse_reward
 python train.py --run_name her_sparse --use_her --sparse_reward
+python train.py --run_name her_sparse_tau_0.0001 --use_her --sparse_reward --soft_tau 0.0001
+python train.py --run_name her_sparse_tau_0.01 --use_her --sparse_reward --soft_tau 0.01
+python train.py --run_name her_sparse_tau_0.1 --use_her --sparse_reward --soft_tau 0.1
